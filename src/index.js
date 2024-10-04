b60ef3e93e31c5e3416bc30916ecb32fbb5935f4
Acatrinei-Bogdan-Stefan <bogdan.z9.acatrinei@gmail.com>
2024-10-04 21:16:12 +0300

    Version 0.02

--- src/index.js
+++ src/index.js
@@ -21,13 +21,14 @@ const config = {
 
 // Variables
 const VELOCITY = 200;
+const PIPES_TO_REMDER = 4;
 const initialBirdPosition = {x:config.width * 0.1, y:config.height / 2};
 const pipeVerticalDistanceRange = [150, 250];
+
+let pipeHorizontalDistance = 0;
 let bird = null;
 let upperPipe = null;
 let lowerPipe = null;
-let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
-let pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance);
 let flapVelocity = 300;
 
 // Loading assetes, such as images, music, animations...
@@ -43,8 +44,19 @@ function create(){
   bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird').setOrigin(0);
   bird.body.gravity.y = 400;
 
-  upperPipe = this.physics.add.sprite(400, pipeVerticalPosition, 'pipe').setOrigin(0, 1);
-  lowerPipe = this.physics.add.sprite(400, upperPipe.y + pipeVerticalDistance, 'pipe').setOrigin(0, 0);
+  for ( let i = 0; i < PIPES_TO_REMDER; i++) {
+    pipeHorizontalDistance += 400;
+    let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
+    let pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance);
+
+    upperPipe = this.physics.add.sprite(pipeHorizontalDistance, pipeVerticalPosition, 'pipe').setOrigin(0, 1);
+    lowerPipe = this.physics.add.sprite(pipeHorizontalDistance, upperPipe.y + pipeVerticalDistance, 'pipe').setOrigin(0, 0);
+
+    upperPipe.body.velocity.x = -200;
+    lowerPipe.body.velocity.x = -200;
+  }
+
+ 
   
   this.input.on('pointerdown', flap);
 
