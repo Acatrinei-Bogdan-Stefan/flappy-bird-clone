c7445f15bb4a3d61dc31754b705eb709ee9776e9
Acatrinei-Bogdan-Stefan <bogdan.z9.acatrinei@gmail.com>
2024-10-15 06:01:36 +0300

    Version 0.06

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -15,6 +15,9 @@ class PlayScene extends Phaser.Scene {
         this.pipeHorizontalDistanceRange = [400, 500];
         this.pipeHorizontalDistance = 0;
         this.flapVelocity = 300;
+
+        this.score = 0;
+        this.scoreText = '';
     }
 
     // Loading assetes, such as images, music, animations...
@@ -29,6 +32,7 @@ class PlayScene extends Phaser.Scene {
         this.createBird();
         this.createPipes();
         this.createColliders();
+        this.createScore();
         this.handleInputs();
     }
 
@@ -43,12 +47,17 @@ class PlayScene extends Phaser.Scene {
 
     createBird(){
         this.bird = this.physics.add.sprite(this.config.startPosition.x, this.config.startPosition.y, 'bird').setOrigin(0);
-        this.bird.body.gravity.y = 400;
+        this.bird.body.gravity.y = 600;
         this.bird.setCollideWorldBounds(true);
     }
 
     createColliders(){
         this.physics.add.collider(this.bird, this.pipes, this.gameOver, null, this);
+    }
+
+    createScore(){
+        this.score = 0;
+        this.scoreText = this.add.text(16, 16, `Score: ${0}`,{ fontSize: '32px',fill: '#000'});
     }
 
     createPipes(){
@@ -99,6 +108,7 @@ class PlayScene extends Phaser.Scene {
           tempPipes.push(pipe);
           if(tempPipes.length === 2){
             this.placePipe(...tempPipes);
+            this.increaseScore();
           }
           }
         })
@@ -116,15 +126,25 @@ class PlayScene extends Phaser.Scene {
     }
       
     gameOver() {
-        // this.bird.x = this.config.startPosition.x;
-        // this.bird.y = this.config.startPosition.y;
-        // this.bird.body.velocity.y = 0;
         this.physics.pause();
         this.bird.setTint(0xEE4824);
+
+        this.time.addEvent({
+            delay:1000,
+            callback: () => {
+                 this.scene.restart();
+            },
+            loop:false
+        })
     }
       
     flap() {
         this.bird.body.velocity.y = -this.flapVelocity;
+    }
+
+    increaseScore(){
+        this.score++;
+        this.scoreText.setText(`Score: ${this.score}`)
     }
 }
 
